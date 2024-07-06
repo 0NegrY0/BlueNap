d5ed89952b2d177f90fc006a4de59a1a6396c07b
Gutok66 <60787635+Gutok66@users.noreply.github.com>
2024-07-06 19:45:53 -0300

    Mudanças
    branch augusto

---
+++ src/Interface/Headers/Interface_Subservice.hpp
@@ -0,0 +1,33 @@
+#ifndef NETWORK_DISCOVERY_HPP
+#define NETWORK_DISCOVERY_HPP
+
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <cstring>
+#include <mutex>
+#include <vector>
+#include <atomic>
+
+#define PORT_DISCOVERY 8888
+#define MAX_BUFFER_SIZE 1024
+#define DISCOVERY_MESSAGE "wakeywakey"
+
+struct Computer {
+    std::string macAddress;
+    std::string ipAddress;
+    int id;
+    bool isServer;
+};
+
+extern std::mutex mtx;
+extern std::atomic<bool> stopSending;
+extern std::atomic<bool> isDiscovered;
+extern std::vector<Computer> computers;
+
+void handleDiscoveryReceiver();
+void handleDiscoverySender();
+
+#endif // NETWORK_DISCOVERY_HPP
---
+++ src/Interface/Interface_Subservice.cpp
@@ -0,0 +1,70 @@
+#include "Headers/Interface_Subservice.hpp"
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cerrno>
+#include <unistd.h>
+
+using namespace std;
+
+vector<Computer> computers; // Vector to store information of the computers
+
+
+// Function to send discovery messages
+void Interface() {
+    bool isServer;
+    int SIZE;
+
+    int index = 0;
+    Computer leader;
+    leader = computers[index];
+    while (!leader.isServer){
+        index++;
+        leader = computers[index];
+    }
+    string input;
+
+    while (true){
+        cout << endl << "========== Leader Machine ==========" << endl;
+        cout << "ID: "<<leader.id<<" MAC Adress:"<<leader.macAddress<<" IP Adress: "<<leader.ipAddress;
+        cout << endl << "========== Clients ==========" << endl;
+        for (int i=0; i<SIZE; i++){
+            if (i != index){
+                cout << "ID: "<<computers[i].id<<" MAC Adress:"<<computers[i].macAddress<<" IP Adress: "<<computers[i].ipAddress<<endl;
+            }
+        }
+        cout << "ID: "<<leader.id<<" MAC Adress:"<<leader.macAddress<<" IP Adress: "<<leader.ipAddress;
+        if (isServer){
+            cout << endl << "You are the Leader" << endl; 
+            cout << "Enter 0 to exit, Enter 1 to wake a client, Enter anything to update" << endl; 
+        }
+        else {
+            cout << endl << "You are a Cliente" << endl << "Enter 0 to exit, Enter anything to update" << endl; 
+        }
+        getline(cin, input);
+        if (input == "0"){
+            return;
+        }
+        if (input == "1" and isServer){
+            cout << "Enter the ID of the client you want to awake: ";
+            getline(cin, input);
+            //acordar (input); FUNCAO DO TIAGO OU EVENTO SEILA
+        }
+        cout <<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
+    }
+    return;
+}
+
+int main() {
+    std::vector<std::thread> threads;
+
+    // Add threads to create interface
+    threads.push_back(std::thread(Interface));
+
+    // Wait for all threads to finish
+    for (auto& th : threads) {
+        th.join();
+    }
+
+    return 0;
+}
