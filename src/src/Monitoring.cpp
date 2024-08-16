57b69a8e3439658f36b9e546473bb15c4d998aee
ThiagoTrautwein <ttrautweinsantos@gmail.com>
2024-08-15 23:02:05 -0300

    csacsadc

--- src/src/Monitoring.cpp
+++ src/src/Monitoring.cpp
@@ -32,6 +32,8 @@ int Monitoring::server() {
             struct sockaddr_in clientAddr = configureAdress(clientIp, clientPort);
             socklen_t clientLen = sizeof(clientAddr);
 
+            cout << "Vou enviar mensagem de monitoramento para: " << clientIp << ":" << clientPort << endl;
+
             if (computers[i].ipAddress == oldServerIP) {
                 strcpy(buffer, NEW_LEADER_MESSAGE);
                 sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&clientAddr, clientLen);
