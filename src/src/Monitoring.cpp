d15d27acbaede4face83e0d624e09999597051fc
thiagotrautwein <ttrautweinsantos@gmail.com>
2024-08-16 10:25:21 -0300

    smfowemofwe

--- src/src/Monitoring.cpp
+++ src/src/Monitoring.cpp
@@ -33,6 +33,7 @@ int Monitoring::server() {
             cout << "ENviando mensagem ao cliente" << clientIp << ":" << clientPort << endl;
 
             if (computers[i].ipAddress == oldServerIP) {
+                setSocketTimeout(sockfd, 1);
                 strcpy(buffer, NEW_LEADER_MESSAGE);
                 sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&clientAddr, clientLen);
                 clientAddr = configureAdress(clientIp, clientPort);
@@ -48,6 +49,7 @@ int Monitoring::server() {
                         mtx.unlock();
                     }
                 }
+                setSocketTimeout(sockfd, TIMEOUT_SEC);
             }
             else {
                 vector<char> send;
