9ec844238557b91f96c64d1a42a0b6fd6301b430
ThiagoTrautwein <ttrautweinsantos@gmail.com>
2024-08-15 21:16:53 -0300

    vmpwvmwopmw

--- src/src/Monitoring.cpp
+++ src/src/Monitoring.cpp
@@ -116,6 +116,8 @@ int Monitoring::server() {
                 }
             }
         }
+        cout << "sai do for";
+        cout << "IsMaster fora do for: " << isMaster << endl;
         sleep(1);
     }
     cout << "sai do while do master";
